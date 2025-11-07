aa5ce2b5f8e6a4815c0a5496e63df0a56046aafe
SASCHADDD <saschadanu23@gmail.com>
2025-11-07 15:14:48 +0700

    membuat function , scaffold

--- app/src/main/java/com/example/pertemuan6/view/Data.kt
+++ app/src/main/java/com/example/pertemuan6/view/Data.kt
@@ -1,2 +1,53 @@
 package com.example.pertemuan6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan6.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Form(
+    jenisK: List<R.string> = listOf("laki-laki","perempuan"),
+    OnsubmitBtnClick : () -> Unit
+){
+    Scaffold (
+        modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(R.string.home), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(R.color.teal_700))
+            )}
+    ){ isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Top,
+            OutlinedTextField(
+                value = "",
+                singleLine =  true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+
+
+            )
+        )
+        }
+    )
+}
