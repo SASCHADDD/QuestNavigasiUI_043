9fb4efbe876f434032bff52bc5aa7235d0cc98cf
SASCHADDD <saschadanu23@gmail.com>
2025-11-07 20:52:51 +0700

    membuat fungsi untuk tombol navigasi back kembali ke beranda

--- app/src/main/java/com/example/pertemuan6/navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/navigasi.kt
@@ -5,6 +5,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.pertemuan6.view.Form
+import com.example.pertemuan6.view.TampilData
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
     Formulirku,
@@ -13,18 +21,37 @@ enum class Navigasi {
 
 @Composable
 fun DataApp(
-    navController: navHostController = remembernavController()
+    navController: NavHostController = rememberNavController()
 ){
-    Scaffold {isiRuang ->
+    Scaffold { isiRuang ->
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier
-                .padding(isiRuang)){
-            composable(route = Navigasi.Formulirku.name)
-
+                .padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                Form(
+                    OnsubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+            TampilData(
+                onBackBtnClick = {
+                    cancelAndBackToFormulir(navController)
+                }
+            )
         }
+    }
 
     }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavController
+){
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
 }
