31e45de3c669e40688fd775986ecd69ab5f539ec
SASCHADDD <saschadanu23@gmail.com>
2025-11-07 18:58:32 +0700

    menyetting padding

--- app/src/main/java/com/example/pertemuan6/navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/navigasi.kt
@@ -1,5 +1,6 @@
 package com.example.pertemuan6
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -20,6 +21,7 @@ fun DataApp(
             startDestination = Navigasi.Formulirku.name,
 
             modifier = Modifier
+                .padding(isiRuang)
         )
     }
 }
